75fa59bf43b30eb4519c3e408f0e55880c564980
rodrigues <erkrodrigs@gmail.com>
2024-01-19 19:47:44 -0300

    desafio 6

--- app.js
+++ app.js
@@ -13,3 +13,10 @@ function perguntarCidadeDoBrasil() {
     let cidadeDigitada = prompt('Digite o nome de uma cidade do Brasil:');
     alert(`Estive em ${cidadeDigitada} e lembrei de você.`);
 };
+
+function somarDoisNumeros(num1, num2) {
+    let num1 = parseInt(prompt('Digite o primeiro número.'));
+    let num2 = parseInt(prompt('Digite o segundo número.'));
+    let soma = num1 + num2;
+    return soma;
+}
