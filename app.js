db216dced96941ec5adce0b0469c4700c610709a
rodrigues <erkrodrigs@gmail.com>
2024-01-19 19:32:37 -0300

    adiciona app.js e modifica index.html na resolução do desafio 4

---
+++ app.js
@@ -0,0 +1,10 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora do desafio';
+
+function consoleClicado() {
+    console.log('O botão foi clicado');
+}
+
+function alerta() {
+    alert('Eu amo JS');
+};
