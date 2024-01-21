e983545d4bd6beb645981c8215b5f3c3e1658a7d
rodrigues <erkrodrigs@gmail.com>
2024-01-21 00:59:43 -0300

    adiciona funcionalidade de titulo h1

--- desafio-1.js
+++ desafio-1.js
@@ -1,5 +1,11 @@
 // Criar uma função que exibe "Olá, mundo!" no console.
 
+// Esta função cria e exibe uma mensagem na tag h1
+function titulo() {
+    let titulo = document.querySelector('h1');
+    titulo.innerHTML = 'Desafio 1';
+};
+
 function helloWorld() {
     console.log('Olá, mundo!');
 };
